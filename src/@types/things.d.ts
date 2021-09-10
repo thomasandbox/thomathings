2c80d98299f44c45de2387be2c16b131518de614
tkeiyama <thomas18thx@gmail.com>
2021-09-10 23:11:44 +0900

    Add the Tag type

--- src/@types/things.d.ts
+++ src/@types/things.d.ts
@@ -1,7 +1,16 @@
+type Tag =
+  | 'Compiler'
+  | 'Design'
+  | 'React'
+  | 'Rust'
+  | 'TypeScript'
+  | 'Web'
+  | 'WebAssembly'
+
 export interface Thing {
   date: string
   title: string
   url: string
   description: string
-  tags: string[]
+  tags: Tag[]
 }
